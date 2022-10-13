8619e8f3f7660114c62b7b537ab7d51451aee435
WILLIDEVELOPER <wilsai1023@gmail.com>
2022-10-13 14:38:40 -0500

    first commit

---
+++ DB/db.sql
@@ -0,0 +1,4 @@
+create table users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL
+);
---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { sqlConection } from "./db.js";
+import { DB_PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    const [rows] = await sqlConection.query("SELECT * FROM users");
+  res.json(rows);
+});
+
+app.get("/ping", async (req, res) => {
+  const [result] = await sqlConection.query(`SELECT "hello world" as RESULT`);
+  res.json(result[0]);
+});
+
+app.get("/create", async (req, res) =>{
+    const result = await sqlConection.query("INSERT INTO users(name) VALUES('john')");
+    res.json(result);
+})
+
+app.listen(DB_PORT);
+console.log("server on port", DB_PORT);
