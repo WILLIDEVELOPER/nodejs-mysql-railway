133c76d15da854b3fab4d26c4c5ee997c934992c
WILLIDEVELOPER <wilsai1023@gmail.com>
2022-10-13 15:49:51 -0500

    hola

--- src/config.js
+++ src/config.js
@@ -7,3 +7,7 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || '1023';
 export const DB_NAME = process.env.DB_NAME || 'pruebita';
 
 export const DB_PORT = process.env.DB_PORT || 3000;
+
+// "devDependencies": {
+//     "nodemon": "^2.0.20"
+//   }
